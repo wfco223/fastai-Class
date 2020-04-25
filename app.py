f659bad92f3879f0e8fc1eda3c30a285f054bd24
William Cox <williamcox@Williams-MacBook-Air.local>
2020-04-25 15:59:56 -0500

    new

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from fastai.vision import *
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 
 @app.route("/post_field", methods=["GET", "POST"])
 def need_input():
-    for key, value in request.form.items():
-        print("key: {0}, value: {1}".format(key, value))
-
-@app.route("/form", methods=["GET"])
-def get_form():
-    return render_template('index.html')
+    url = request.args['url']
    
 @app.route('/')
 def hello_world():
