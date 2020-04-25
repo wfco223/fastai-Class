b6db7c73dd88b1ca52985e83154ac085fa5f1d69
wfco223 <38415099+wfco223@users.noreply.github.com>
2020-04-25 15:21:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
-from fastai.vision import *
-
+from fastai.vision import learn, load_learner
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import requests
 
 app = Flask(__name__)
 
+@app.route("/post_field", methods=["GET", "POST"])
+def need_input():
+    for key, value in request.form.items():
+        print("key: {0}, value: {1}".format(key, value))
+
+@app.route("/form", methods=["GET"])
+def get_form():
+    return render_template('index.html')
+   
 @app.route('/')
 def hello_world():
    return 'Hello, World!'
+
+def show_path():
+   path = Path(os.getcwd())
+   return(path)
+
+   
