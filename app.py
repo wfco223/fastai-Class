8da5ea7ac8daeed364f5b61f8896cf3ecafc274a
William Cox <williamcox@Williams-MacBook-Air.local>
2020-04-25 17:28:53 -0500

    last?

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from fastai.vision import *
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
 
-@app.route("/post_field", methods=["GET", "POST"])
-def need_input():
-    url = request.args['url']
-   
 @app.route('/')
-def hello_world():
-   return 'Hello, World!'
+def get_url():
+	return(render_template('get_url.html'))
 
-def show_path():
-   path = Path(os.getcwd())
-   return(path)
-
+@app.route('/show_result', methods=['POST', 'GET'])
+def show_result():
+	if request.method == 'POST':
+		url = request.form['url']
+		path = Path(os.getcwd())
+		learn = load_learner(path)
+		import requests
+		f = open('00000001.jpg','wb')
+		link = input()
+		f.write(requests.get(link).content)
+		prediction = learn.predict(open_image(Path(os.getcwd())/'00000001.jpg'))
+		f.close()
+		return(render_template('show_result.html', url = url, path = path, prediction = prediction))
    
+app.run(host = '0.0.0.0', port = 5000)   
